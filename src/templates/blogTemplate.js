8aa9200eab5c3a2f51ae19fc221929b08af04757
Ricardo Ramirez <ricardo.ramirezlara@gmail.com>
2020-05-04 22:12:49 -0600

    Add Twitter and Githu link to blog

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -4,8 +4,9 @@ import BlogHeader from '../components/blogHeader';
 import IndexLayout from '../components/indexLayout';
 
 export default ({ data }) => {
-  const { frontmatter, excerpt, html } = data.markdownRemark;
-  const { tags, title, date, excerpt: extract } = frontmatter;
+  const { frontmatter, excerpt, html, fields } = data.markdownRemark;
+  const { tags, title, date, excerpt: extract, path } = frontmatter;
+  const { slug } = fields;
 
   return (
     <IndexLayout>
@@ -17,6 +18,24 @@ export default ({ data }) => {
           extract={extract || excerpt}
         />
         <div className="markdown" dangerouslySetInnerHTML={{ __html: html }} />
+        <p>
+          <a
+            href={`http://twitter.com/share?text=${title}&amp;url=https://ricardoerl.com${path}&via=ricardoerl`}
+            className="underline text-primary"
+          >
+            Share on Twitter
+          </a>{' '}
+          &bull;{' '}
+          <a
+            href={`https://github.com/ricardoerl/ricardoerl.com/blob/master/src/posts/${slug.replace(
+              /\//g,
+              '',
+            )}.md`}
+            className="underline text-primary"
+          >
+            Edit on GitHub
+          </a>
+        </p>
       </div>
     </IndexLayout>
   );
@@ -27,11 +46,15 @@ export const query = graphql`
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
       excerpt(pruneLength: 250)
+      fields {
+        slug
+      }
       frontmatter {
         title
         tags
         date(formatString: "MMMM DD, YYYY")
         excerpt
+        path
       }
     }
   }
