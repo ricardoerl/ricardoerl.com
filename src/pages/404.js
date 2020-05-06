e43acae39d1f4d58f35619dc98a9745b5471f22a
Ricardo Ramirez <ricardo.ramirezlara@gmail.com>
2020-05-05 22:57:47 -0600

    Update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 import IndexLayout from '../components/indexLayout';
 
 const NotFoundPage = () => (
   <IndexLayout>
-    <h1>404</h1>
+    <div className="grid grid-cols-6 gap-4">
+      <div className="col-span-6 md:col-start-2 sm:col-span-1">
+        <h2 className="text-4xl mb-4">404</h2>
+      </div>
+      <div className="col-span-6 sm:col-span-3">
+        <Link to="/" className="text-primary">
+          Regresar al inicio
+        </Link>
+      </div>
+    </div>
   </IndexLayout>
 );
 
